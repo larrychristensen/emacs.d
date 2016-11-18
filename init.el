5ab8756c7e74e8369959f88eaf163ececa479a53
Larry Christensen <larry.christensen@clientsuccess.com>
2016-11-18 14:09:02 -0700

    More configuration

--- init.el
+++ init.el
@@ -5,44 +5,48 @@
 (package-initialize)
 
 
-;;; Install Packages
+;; Execution Path
+(add-to-list 'exec-path "~/bin")
+
+
+;;; Plugin-Specific Configurations
+;; Cider
 (unless (package-installed-p 'cider)
   (package-install 'cider))
 
+;; Configure Magit
 (unless (package-installed-p 'magit)
   (package-install 'magit))
 
-(unless (package-installed-p 'solarized-theme)
-  (package-install 'solarized-theme))
+(global-set-key (kbd "C-x g") 'magit-status)
 
+;; Paredit
 (unless (package-installed-p 'paredit)
   (package-install 'paredit))
 
+(add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+(add-hook 'clojure-mode-hook #'paredit-mode)
+
+;; Rainbow Delimiters
+(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
+
+;; Global Hooks
 (unless (package-installed-p 'company)
   (package-install 'company))
 
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; Expand Region
 (unless (package-installed-p 'expand-region)
   (package-install 'expand-region))
 
+(require 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;; Which Key
 (unless (package-installed-p 'which-key)
   (package-install 'which-key))
 
 
-;;; Global Hooks
-(add-hook 'after-init-hook 'global-company-mode)
-
-
-;;; Clojure Hooks
-
-
-;; Plugin-Specific Configurations
-;; Execution Path
-(add-to-list 'exec-path "~/bin")
-
-;; Load Solarized Theme
-(load-theme 'solarized-dark t)
-
-;; Configure Magit
-(global-set-key (kbd "C-x g") 'magit-status)
 
 
