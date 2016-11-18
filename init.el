0a1e1279221bd94413b3d34cf12a84c1e25fc149
Larry Christensen <larry.christensen@clientsuccess.com>
2016-11-18 14:48:54 -0700

    Config finally working

--- init.el
+++ init.el
@@ -5,8 +5,24 @@
 (package-initialize)
 
 
+;; Full Screen
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(initial-frame-alist (quote ((fullscreen . maximized))))
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters which-key solarized-theme paredit magit expand-region company cider))))
+
 ;; Execution Path
 (add-to-list 'exec-path "~/bin")
+
+(setq default-directory "/Users/larry/Documents/code/")
 
 
 ;;; Plugin-Specific Configurations
@@ -24,17 +40,14 @@
 (unless (package-installed-p 'paredit)
   (package-install 'paredit))
 
-(add-hook 'emacs-lisp-mode-hook #'paredit-mode)
 (add-hook 'clojure-mode-hook #'paredit-mode)
+(add-hook 'cider-repl-mode-hook #'paredit-mode) 
 
 ;; Rainbow Delimiters
+(unless (package-installed-p 'rainbow-delimiters)
+  (package-install 'rainbow-delimiters))
+
 (add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
-
-;; Global Hooks
-(unless (package-installed-p 'company)
-  (package-install 'company))
-
-(add-hook 'after-init-hook 'global-company-mode)
 
 ;; Expand Region
 (unless (package-installed-p 'expand-region)
@@ -46,7 +59,18 @@
 ;; Which Key
 (unless (package-installed-p 'which-key)
   (package-install 'which-key))
+(require 'which-key)
+(which-key-mode)
 
+;; Company Mode
+(unless (package-installed-p 'company)
+  (package-install 'company))
 
+(add-hook 'after-init-hook 'global-company-mode)
 
-
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
